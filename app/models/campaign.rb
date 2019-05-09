fed065c6327fd9232e7c8b278b29818d2b6cdd51
Matt Readout <matt.readout@gmail.com>
2019-05-09 18:07:17 -0400

    add #estimated_in_home_date and #cost to campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,25 @@
 class Campaign < ApplicationRecord
   belongs_to :client
+
+	def estimated_in_home_date
+	  mailing_days = {
+			'First Class' => 3,
+			'Standard Mail' => 7
+		}
+		return self.mailing_date + mailing_days[self.postage_type]
+	end
+
+	def cost
+	  postage_cost = {
+			'First Class' => 0.45,
+			'Standard Mail' => 0.70
+		}
+		paper_cost = {
+			'4x6' => 1,
+			'5x7' => 1.25,
+			'6x9' => 2,
+			'8x10' => 3
+		}
+		return postage_cost[self.postage_type] * paper_cost[self.paper_format] * self.recipient_count
+	end
 end
