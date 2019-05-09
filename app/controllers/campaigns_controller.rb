ccd8e716bb2fbbe627d16e6e6442420b3166e96d
Matt Readout <matt.readout@gmail.com>
2019-05-09 17:16:01 -0400

    campaigns#show

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,25 @@
 class CampaignsController < ApplicationController
+	before_action :find_campaign, only: [:show]
+
+	def show
+	end
+
+	def new
+	  @campaign = Campaign.new
+	end
+
+	def create
+	  @campaign = Campaign.create(campaign_params)
+		if @campaign.valid?
+			redirect_to campaign_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def find_campaign
+	  @campaign = Campaign.find(params[:id])
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 clients = Client.create([{ name: 'Google' }, { name: 'Apple' }, { name: 'Facebook' }])
+
+Campaign.create([{ name: 'New Gmail', recipient_count: 100000, mailing_date: Date.new(2019, 5, 9), paper_format: '4x6', postage_type: 'First Class', client: Client.first }])
