5b207f79a2e60b4c931b79c292c0f0267d55360a
Matt Readout <matt.readout@gmail.com>
2019-05-09 16:05:32 -0400

    create seeds & define client model, controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,17 @@
 class ClientsController < ApplicationController
+	before_action :find_client, only: [:show]
+
+	def index
+
+	end
+
+	def show
+	  @campaigns = @client.campaigns
+	end
+
+	private
+
+	def find_client
+	  @client = Client.find(params[:id])
+	end
 end
